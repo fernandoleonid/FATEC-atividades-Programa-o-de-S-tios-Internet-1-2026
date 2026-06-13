ee2e666546247b6a9590893ad5393548962c94b4
Robson Cassiano <robcassiano@gmail.com>
2026-06-13 01:36:59 -0300

    Delete P2 directory

--- P2/js/views/about.js
+++
@@ -1,5 +0,0 @@
-export const aboutView = async () => `
-    <div class="about-container">
-        <h1>Sobre a Biblioteca Digital</h1>
-        <p>Este é um projeto Single Page Application desenvolvido com Vanilla JS para consumo de API REST.</p>
-    </div>`;
